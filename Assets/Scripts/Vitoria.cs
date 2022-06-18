eebbf114e88d109dd803559e98435af479c4420e
RogueArch <caiomendes2704@gmail.com>
2022-06-18 00:55:57 -0300

    Shine Finalizado!!!!!!

--- Assets/Scripts/Vitoria.cs
+++ Assets/Scripts/Vitoria.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Vitoria : MonoBehaviour
 {
     public void Menu()
     {
         SceneManager.LoadScene("Menu");
     }
+
     public void Quit()
     {
         Application.Quit();
